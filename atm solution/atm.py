16543eb327c61c0fd533e953812c4e1036179720
Unknown <samer.elhamdoo@gmail.com>
2018-03-31 17:04:12 +0300

    add class atm

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,36 +1,47 @@
-def checkMoney(request,money):
-    result = request
-    if request > money:
-        print("Money is not enough !!\n the request is: {} \n the money is: {}".format(request,money))
-    else:
-        while request > 0:
-            if request >= 100:
-                print("give ",100)
-                request -= 100
-            elif request >= 50:
-                print("give ",50)
-                request -=50
-            elif request >= 20:
-                print("give ",20)
-                request -= 20
-            elif request >= 10:
-                print("give ",10)
-                request -= 10
-            elif request >= 5:
-                print("give ",5)
-                request -= 5
-            elif request >= 2:
-                print("give ",2)
-                request -= 2
-            else:
-                print("give ",1)
-                request-=1
-        print(" the request is: {} \n the money is:"
-              " {}\n the remaining money is: {}".format(result,money,(money-result)))
-    print("="*100)
-    balance = (money - result)
-    return balance
-balance = 1500
-balance = checkMoney(399,balance)
-balance = checkMoney(201,balance)
-balance = checkMoney(356,balance)
+class ATM:
+    def __init__(self, balance, bank_name):
+        self.balance = balance
+        self.bank_name = bank_name
+
+    def withdraw(self, request):
+        print("welcome to: {}\nCurrent balance: {}".format(self.bank_name, self.balance))
+        print("="*50)
+        result = request
+        if request > self.balance:
+            print("Money is not enough !!\n the request is: {} \n the money is: {}".format(request, self.balance))
+        else:
+            while request > 0:
+                if request >= 100:
+                    print("give ", 100)
+                    request -= 100
+                elif request >= 50:
+                    print("give ", 50)
+                    request -= 50
+                elif request >= 20:
+                    print("give ", 20)
+                    request -= 20
+                elif request >= 10:
+                    print("give ", 10)
+                    request -= 10
+                elif request >= 5:
+                    print("give ", 5)
+                    request -= 5
+                elif request >= 2:
+                    print("give ", 2)
+                    request -= 2
+                else:
+                    print("give ", 1)
+                    request -= 1
+            print("the request is: {} \nthe money is:"
+                  " {}\nthe remaining money is: {}".format(result, self.balance, (self.balance - result)))
+        print("=" * 50)
+        self.balance = (self.balance - result)
+        return self.balance
+balanc1 =600
+balanc2 = 1200
+atm1 = ATM(balanc1, "bank albaraka")
+atm2 = ATM(balanc2, "bank ziraat")
+atm1.withdraw(559)
+atm1.withdraw(440)
+atm2.withdraw(685)
+atm2.withdraw(254)
