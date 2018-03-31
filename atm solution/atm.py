cf90dd97f70a798169e521076e9ff6d57bfe3b91
Unknown <samer.elhamdoo@gmail.com>
2018-03-31 16:24:01 +0300

    change function

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -5,30 +5,32 @@ def checkMoney(request,money):
     else:
         while request > 0:
             if request >= 100:
-                print(100)
+                print("give ",100)
                 request -= 100
-            elif request >= 50 and request < 100:
-                print(50)
+            elif request >= 50:
+                print("give ",50)
                 request -=50
-            elif request >= 20 and request < 50:
-                print(20)
+            elif request >= 20:
+                print("give ",20)
                 request -= 20
-            elif request >= 10 and request < 20:
-                print(10)
+            elif request >= 10:
+                print("give ",10)
                 request -= 10
-            elif request >= 5 and request < 10:
-                print(5)
+            elif request >= 5:
+                print("give ",5)
                 request -= 5
-            elif request >= 2 and request < 5:
-                print(2)
+            elif request >= 2:
+                print("give ",2)
                 request -= 2
             else:
-                print(1)
+                print("give ",1)
                 request-=1
         print(" the request is: {} \n the money is:"
               " {}\n the remaining money is: {}".format(result,money,(money-result)))
     print("="*100)
-
-checkMoney(399,800);
-checkMoney(1000,800);
-checkMoney(257,800);
+    balance = (money - result)
+    return balance
+balance = 1500
+balance = checkMoney(399,balance)
+balance = checkMoney(201,balance)
+balance = checkMoney(356,balance)
