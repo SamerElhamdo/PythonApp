ce1222605129b7b7fc378be31a60bc17b40a1714
Unknown <samer.elhamdoo@gmail.com>
2018-03-31 18:50:58 +0300

    add folder froums
    تمت أضافة مجلد المشروع وانشاء ملف رئيسي يستدعي فئات من ملف الفئات
    وتم انشاء فئتين في ملف الفئات  واحدة للاعضاء و واحدة للمنشورات 
    وهذا لتنفيذ متطلبات الدرس في هذا الرابط : http://www.coretabs.net/fullstack/

---
+++ Forums/main.py
@@ -0,0 +1,10 @@
+from models import Member, Post
+
+member1 = Member("samer", 27)
+member2 = Member("ahmed", 22)
+
+post1 = Post("hi body1", "how are you1")
+post2 = Post("hi body2", "how are you2")
+post3 = Post("hi body3", "how are you3")
+
+print(member1, member2,post1,post2,post3)
