3ac8b015aca2b797379ded83a79022c606e54bb8
Unknown <samer.elhamdoo@gmail.com>
2018-04-13 03:02:06 +0300

    Filtering Part-1

--- Forums/stores.py
+++ Forums/stores.py
@@ -1,89 +1,74 @@
-#entity_exists
-#اكتب الدالة delete.
-
-
-class Member_store:
+class MemberStore:
     members = []
     last_id = 1
 
+    def add(self, member):
+        member.id = MemberStore.last_id
+        MemberStore.members.append(member)
+        MemberStore.last_id += 1
 
+    def get_all(self):
+        return MemberStore.members
 
-    def Add(self, member):
-        member.id = Member_store.last_id
-        if self.entity_exists(member):
-            self.delete(member.id)
-        Member_store.members.append(member)
-        Member_store.last_id += 1
-
-
-
-
-    def get_All(self):
-        for member in Member_store.members:
-            print("name is: {}\nage is: {}\nid is: {}\n{}".format(member.name, member.age, member.id, ("="*50)))
-
-
-
-    def getById(self, id):
-        all_members = Member_store.members
+    def get_by_id(self, id):
         result = None
-        for member in all_members:
-            if member.id == id:
-                result = member
+        all_members = self.get_all()
+        for e in all_members:
+            if e.id == id :
+                result = e
         return result
-
-
 
     def entity_exists(self, member):
         result = False
-        if self.getById(member.id):
+        if self.get_by_id(member.id):
             result = True
         return result
 
-
-
     def delete(self, id):
-        member= self.getById(id)
-        Member_store.members.remove(member)
+        member = self.get_by_id(id)
+        MemberStore.members.remove(member)
+
+    def update(self, member):
+        all_members = self.get_all()
+        for i, m in enumerate(all_members):
+            if member.id == m.id:
+                all_members[i] = member
+                break
 
 
-
-
-class Post_store:
-    Posts = []
+class PostStore:
+    posts = []
     last_id = 1
 
-    def Add(self, post):
-        post.id = Post_store.last_id
-        if self.entity_exists(post):
-            self.delete(post.id)
-        Post_store.Posts.append(post)
-        Post_store.last_id += 1
+    def add(self, post):
+        post.id = PostStore.last_id
+        PostStore.posts.append(post)
+        PostStore.last_id += 1
 
+    def get_all(self):
+        return PostStore.posts
 
-    def get_All(self):
-        for post in Post_store.Posts:
-            print(post.title, post.content, post.id)
-
-
-    def getById(self, id):
-        all_posts = Post_store.Posts
+    def get_by_id(self, id):
         result = None
-        for post in all_posts:
-            if post.id == id:
-                result = post
+        all_posts = self.get_all()
+        for e in all_posts:
+            if e.id == id :
+                result = e
         return result
-
-
 
     def entity_exists(self, post):
         result = False
-        if self.getById(post.id):
+        if self.get_by_id(post.id):
             result = True
         return result
 
-
-
     def delete(self, id):
-        post = self.getById(id)
-        Post_store.Posts.remove(post)
+        post = self.get_by_id(id)
+        PostStore.posts.remove(post)
+
+    def update(self, post):
+        all_posts = self.get_all()
+        for i, p in enumerate(all_posts):
+            if post.id == p.id:
+                all_posts[i] = post
+                break
