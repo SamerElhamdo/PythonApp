50ba5c50b1055822ca5472d78744092a0b204a0e
Unknown <samer.elhamdoo@gmail.com>
2018-04-14 20:09:42 +0300

    Templates with Jinja

--- Forums/app.py
+++ Forums/app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from Forums import models, stores
+
 
 app = Flask(__name__)
+
+post_store = stores.PostStore()
+member1 = models.Post("Life", "Life is alawys great")
+member2 = models.Post("Sunshine", "Sunshine is amazing")
+post_store.add(member1)
+post_store.add(member2)
+
 @app.route("/")
 @app.route("/index")
 
 def home():
-    return render_template("index.html")
+    posts = post_store.get_all()
+    return render_template('index.html', posts=posts)
+
 
 app.run()
