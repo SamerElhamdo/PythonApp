87e9d05bfc312e1161d916420f06aa0dcebb9a30
Unknown <samer.elhamdoo@gmail.com>
2018-05-07 17:32:03 +0300

    Merge branch 'release/1.0.3'

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request,redirect,url_for
 from app import models
 from app import app, member_store, post_store
+from flask import jsonify
 
 @app.route("/")
 @app.route("/index")
@@ -39,3 +40,15 @@ def edit_topic(id):
         return redirect(url_for('home'))
     else:
         return render_template("edit.html", my_post = my_post)
+
+@app.route("/api/topic/all")
+def topic_get_all():
+    posts = [post.__dict__() for post in post_store.get_all()]
+    return jsonify(posts)
+
+@app.route("/api/topic/add", methods = ["POST"])
+def topic_create():
+    request_data = request.get_json()
+    new_post = models.Post(request_data["title"], request_data["content"])
+    post_store.add(new_post)
+    return jsonify(new_post.__dict__())
